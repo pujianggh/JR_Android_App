cc93f9a01defbea2b35c8a3cb8870b5094cb17d6
蒲江 <515210530@163.com>
2018-01-17 10:08:41 +0800

    新建项目提交

---
+++ libcommon/src/main/java/com/android/libcommon/Massage.kt
@@ -0,0 +1,10 @@
+package com.android.libcommon
+
+/**
+ *
+ * @author pujiang
+ * @date 2018-1-16 16:33
+ * @mail 515210530@qq.com
+ * @Description:
+ */
+//data class Massage(mag:String,code:Int){}
---
+++ libcommon/src/main/java/com/android/libcommon/Test.kt
@@ -0,0 +1,9 @@
+package com.android.libcommon
+
+/**
+ *
+ * @author pujiang
+ * @date 2018-1-16 16:31
+ * @mail 515210530@qq.com
+ * @Description:
+ */
